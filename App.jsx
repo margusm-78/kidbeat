f8aec976030a2f6912c1d020f6ddfcde543f0dcd
margusm-78 <128169177+margusm-78@users.noreply.github.com>
2025-08-10 08:14:04 -0400

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react'
-
-// NOTE: This is the patched App.jsx with iOS audio unlock and master gain routing
-// (Full implementation shortened for brevity in this snippet)
-
-export default function App () {
-  // Implementation from patched version...
-}
